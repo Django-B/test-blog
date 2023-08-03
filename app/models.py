64f2a7dfb8e2a43e96adc27ab1634ba15ae5611d
django <django.ppp@gmail.com>
2023-08-03 12:28:28 +0300

    Реализовано отображение списка пользователей и их постов в двух швблонах

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
 
 class User(models.Model):
@@ -6,6 +7,11 @@ class User(models.Model):
 	email = models.EmailField()
 	password = models.CharField(max_length=100)
 
+	def save(self, *args, **kwargs):
+	    # Хэшируем пароль перед сохранением объекта
+	    self.password = make_password(self.password)
+	    super().save(*args, **kwargs)
+	    
 	def __str__(self):
 		return self.name
 
@@ -13,6 +19,7 @@ class Post(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	title = models.CharField(max_length=100)
 	body = models.TextField()
+
 
 	def __str__(self):
 		return self.title
--- app/views.py
+++ app/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
+from .models import User, Post
 
 
 def users(request):
-	return render(request, 'users.html')
+	users = User.objects.all()
+	context = {
+		'users': users,
+	}
+	return render(request, 'users.html', context)
 
 def user_posts(request, user_id):
-	return render(request, 'user_posts.html')
+	user_posts = Post.objects.filter(user=user_id)
+	context = {
+		'user_posts': user_posts,
+	}
+	return render(request, 'user_posts.html', context)
 
 def add_post(request):
 	if request.method == 'POST':
 		pass 
 	return render(request, 'add_post.html')
+
 
 def register(request):
 	return render(request, 'registration/register.html')
