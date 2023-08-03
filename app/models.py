9c2516050c605a0d488da9fc776f924d322f3bc8
django <django.ppp@gmail.com>
2023-08-03 11:55:05 +0300

    Исправлены ошибки из-за опечаток

--- app/models.py
+++ app/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 
 class User(models.Model):
-	name = models.CharField(max_width=100, unique=True)
+	name = models.CharField(max_length=100, unique=True)
 	email = models.EmailField()
-	password = models.CharField(max_width=100)
+	password = models.CharField(max_length=100)
 
 	def __str__(self):
 		return self.name
 
 class Post(models.Model):
-	user = models.FoareignKey(User, on_delete=models.CASCADE)
-	title = models.CharField(max_width=100)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	title = models.CharField(max_length=100)
 	body = models.TextField()
 
 	def __str__(self):
